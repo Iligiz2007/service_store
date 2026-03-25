a1394d928b3e384f17b43e561eef67b7e8d6a070
Iligiz <iligiz2007@gmail.com>
2026-03-25 15:47:39 +0500

    feat: add user and shop apps with models, add gitignore

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator
 # Create your models here.
+class Service(models.Model):
+    title = models.CharField(
+        max_length=255,
+        verbose_name="Название",
+        help_text="Назоваите ваш вид услуг")
+    
+    description = models.TextField(
+        verbose_name="Описание",
+        help_text="Описание ваших услуг")
+    
+    price = models.IntegerField (
+        verbose_name="Цена",
+        help_text="Стоимость",
+        validators=[MinValueValidator(100)])
+    
+
