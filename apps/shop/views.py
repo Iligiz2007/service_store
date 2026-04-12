9c3a23d0dff9d7921dee1c60e0da33fe21fa0017
Iligiz <iligiz2007@gmail.com>
2026-04-12 15:35:35 +0500

    add dizain

--- apps/shop/views.py
+++ apps/shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from django.views.generic import CreateView,TemplateView,ListView
+from django.views.generic import CreateView,TemplateView,ListView,DetailView
 from .forms import FormService
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -20,9 +20,14 @@ class ViewsService(LoginRequiredMixin,CreateView):
 class ViewsIndex(TemplateView):
     template_name = 'index.html'
 
+class ViewsDetialService(DetailView):
+    model = Service
+    template_name = 'shop/detail_service.html'
+    context_object_name = "service"
 
 
 class ViewsListService(ListView):
     model = Service
     template_name = "shop/list_service.html"
     context_object_name = "service"
+    
