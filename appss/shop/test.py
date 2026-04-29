3b78c63baba1e981f39c6330d060244b5009bbe2
Iligiz <iligiz2007@gmail.com>
2026-04-29 22:03:46 +0500

    add test

--- appss/shop/test.py
+++ appss/shop/test.py
@@ -1,22 +1,87 @@
-from django.test import TestCase
-from django.test import SimpleTestCase
+from django.test import TestCase, SimpleTestCase
+from django.test import Client
+from django.views.generic import TemplateView
+from . import views
 
-#GET Запросы
+
+# GET Запросы
 class HomePageGetTests(SimpleTestCase):
-    def test_url_access(self):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
         url = '/'
-        response = self.client.get(url)
-        self.assertEqual(response.status_code,200) 
+        client = Client()
+        cls.response = client.get(url)
+
+    def test_url_access(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_url_name(self):
+        self.assertEqual(self.response.resolver_match.url_name, 'home')
+
+    def test_url_namespace(self):
+        self.assertEqual(self.response.resolver_match.namespace, 'shop') 
+
+    def test_view_name(self):
+        self.assertEqual(self.response.resolver_match.func.view_class, views.ViewsIndex)
+
+    def test_template_name(self):
+        self.assertTemplateUsed(self.response, 'index.html')
+
+    def text_base_template_name(self):
+        self.assertTemplateUsed(self.response, 'index.html')
+    
+
 
 class ListServicePageGetTests(TestCase):
-    def test_url_access(self):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
         url = '/list_service/'
-        response = self.client.get(url)
-        self.assertEqual(response.status_code,200)
+        client = Client()
+        cls.response = client.get(url)
 
-#Если пользователь не заригистрирван
-class FormServacePageGetTests(SimpleTestCase):
     def test_url_access(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_url_name(self):
+        self.assertEqual(self.response.resolver_match.url_name, 'list_service')
+
+    def test_url_namespace(self):
+        self.assertEqual(self.response.resolver_match.namespace, 'shop') 
+
+    def test_view_name(self):
+        self.assertEqual(self.response.resolver_match.func.view_class, views.ViewsListService)  
+
+    def test_template_name(self):
+        self.assertTemplateUsed(self.response, 'shop/list_service.html')
+
+    def text_base_template_name(self):
+        self.assertTemplateUsed(self.response, 'index.html')
+# Если пользователь не зарегистрирован
+
+class FormServicePageGetTests(SimpleTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
         url = '/form_servace/'
-        response = self.client.get(url)
-        self.assertEqual(response.status_code,302)
+        client = Client()
+        cls.response = client.get(url)
+
+    def test_url_access(self):
+        self.assertEqual(self.response.status_code, 302) 
+
+    def test_url_name(self):
+        self.assertEqual(self.response.resolver_match.url_name, 'form_servace_name')  
+
+    def test_url_namespace(self):
+        self.assertEqual(self.response.resolver_match.namespace, 'shop')  
+
+    def test_view_name(self):
+        self.assertEqual(self.response.resolver_match.func.view_class, views.ViewsService)  
+
+    def test_redirect_url(self):
+        self.assertRedirects(self.response, '/user/login/?next=/form_servace/')
+
+    def text_base_template_name(self):
+        self.assertTemplateUsed(self.response, 'index.html')
