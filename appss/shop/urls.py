aa258788efa2f92d469e6252cb44fbfd574d047c
Iligiz <iligiz2007@gmail.com>
2026-04-30 16:29:33 +0500

    add status user dizain

--- appss/shop/urls.py
+++ appss/shop/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import ViewsService,ViewsIndex,ViewsListService,ViewsDetialService
-app_name = 'shop'
 urlpatterns = [
     path('detail_servase/<slug:slug>/',ViewsDetialService.as_view(),name="detail_service"),
     path('list_service/',ViewsListService.as_view(),name='list_service'),
