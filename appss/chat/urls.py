50dfd5f7590d4edc48b0775089e43a7c617f592d
Iligiz2007 <iligiz2007@gmail.com>
2026-06-01 16:05:50 +0500

    add offers view

--- appss/chat/urls.py
+++ appss/chat/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ViewsCreateOfferTask,ViewsCreateOfferService
+from .views import ViewsCreateOfferTask,ViewsCreateOfferService,ViewsListOfferTask
 
 urlpatterns = [
     path('create_offer/task/<int:task_id>/',ViewsCreateOfferTask.as_view(),name='create_task_offer'),
     path('create_offer/service/<int:task_id>/',ViewsCreateOfferService.as_view(),name='create_service_offer'),
+    path('list_task/offers/<slug:task_slug>/', ViewsListOfferTask.as_view(), name='list_offers_task'),
 ]
